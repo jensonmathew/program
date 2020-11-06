e1cecd3b7f6cece47b13f7544d82dcffbca80678
Sony K Sunny <sony.sunny3@t-mobile.com>
2020-11-06 13:14:52 +0000

    [ci-skip]Add new file SecurityUserDetails.java

---
+++ src/main/java/com/tmobile/edp/hello/domain/SecurityUserDetails.java
@@ -0,0 +1,77 @@
+/**
+ * 
+ */
+package com.tmobile.edp.hello.domain;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.tmobile.edp.hello.repository.model.Featureflags;
+
+/**
+ * @author Sony K Sunny
+ *
+ */
+public class SecurityUserDetails implements UserDetails {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String userName;
+	private String password;
+	private boolean isActive;
+	private List<GrantedAuthority> authorities;
+	
+	public SecurityUserDetails(Featureflags user) {
+		this.userName = user.getUser();
+		this.password = new BCryptPasswordEncoder().encode(user.getPassword());
+		this.isActive = user.isActive();
+		this.authorities = Arrays.stream(user.getRoles().split(","))
+				.map(SimpleGrantedAuthority::new)
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return this.authorities;
+	}
+
+	@Override
+	public String getPassword() {
+		return this.password;
+	}
+
+	@Override
+	public String getUsername() {
+		return this.userName;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return this.isActive;
+	}
+	
+}
