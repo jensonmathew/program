a67d3d52a236fe6d599f051bba9836c857b04749
Sony K Sunny <sony.sunny3@t-mobile.com>
2020-11-08 16:36:09 +0000

    Sonar Fix: Define a constant instead of duplicating this literal 10 times

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,13 +10,13 @@ roles VARCHAR2(15) DEFAULT NULL);
 INSERT INTO featureflags
 (id, user, email, password, active, roles) 
 VALUES
-(101, 'testuser1','testuser1@t-mobile.com', 'password', true, 'user'),
-(102, 'testuser2','testuser2@t-mobile.com', 'password', true, 'user'), 
-(103, 'testuser3','testuser3@t-mobile.com', 'password', true, 'user'),
-(104, 'testuser4','testuser4@t-mobile.com', 'password', true, 'user'),
-(105, 'testuser5','testuser5@t-mobile.com', 'password', true, 'user'), 
-(106, 'testuser6','testuser6@t-mobile.com', 'password', true, 'user'),
-(107, 'testuser7','testuser7@t-mobile.com', 'password', true, 'user'),
-(108, 'testuser8','testuser8@t-mobile.com', 'password', true, 'user'),
-(109, 'testuser9','testuser9@t-mobile.com', 'password', true, 'user'),
-(110, 'testuser10','testuser10@t-mobile.com', 'password', true, 'user');
+(101, 'testuser1','testuser1@t-mobile.com', 'password1', true, 'user'),
+(102, 'testuser2','testuser2@t-mobile.com', 'password2', true, 'user'), 
+(103, 'testuser3','testuser3@t-mobile.com', 'password3', true, 'user'),
+(104, 'testuser4','testuser4@t-mobile.com', 'password4', true, 'user'),
+(105, 'testuser5','testuser5@t-mobile.com', 'password5', true, 'user'), 
+(106, 'testuser6','testuser6@t-mobile.com', 'password6', true, 'user'),
+(107, 'testuser7','testuser7@t-mobile.com', 'password7', true, 'user'),
+(108, 'testuser8','testuser8@t-mobile.com', 'password8', true, 'user'),
+(109, 'testuser9','testuser9@t-mobile.com', 'password9', true, 'user'),
+(110, 'testuser10','testuser10@t-mobile.com', 'password10', true, 'user');
