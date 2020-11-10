55b9b1424a58b2efc69052b044f0c6512ba4967d
Sony K Sunny <sony.sunny3@t-mobile.com>
2020-11-10 10:22:10 +0000

    [ci-skip] new class for exception

---
+++ src/main/java/com/tmobile/edp/hello/exception/HelloworldException.java
@@ -0,0 +1,13 @@
+package com.tmobile.edp.hello.exception;
+
+public class HelloworldException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public HelloworldException(String message, Exception exception) {
+		super(message, exception);
+	} 
+
+}
