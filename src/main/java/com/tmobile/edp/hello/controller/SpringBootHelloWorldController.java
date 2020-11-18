789b103d981f3c9a6c04203e870f9427b41286c5
Sony K Sunny <sony.sunny3@t-mobile.com>
2020-11-18 17:24:41 +0000

    Fix sonar scan by removes unused imports

--- src/main/java/com/tmobile/edp/hello/controller/SpringBootHelloWorldController.java
+++ src/main/java/com/tmobile/edp/hello/controller/SpringBootHelloWorldController.java
@@ -1,20 +1,13 @@
 package com.tmobile.edp.hello.controller;
 
-import com.tmobile.edp.hello.exception.HelloworldException;
-
 import java.security.Principal;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.tmobile.edp.hello.exception.HelloworldException;
 
 import no.finn.unleash.DefaultUnleash;
 import no.finn.unleash.Unleash;
