c1c08f4fde8e46f938d8d7689465d3de766a9c2d
Vipan Singla <vipan.singla2@t-mobile.com>
2022-03-03 18:59:40 +0000

    Adding readiness and liveness check endpoints. These are unsecured as k8s...

--- src/main/java/com/tmobile/cdp/hello/configuration/SecurityConfiguration.java
+++ src/main/java/com/tmobile/cdp/hello/configuration/SecurityConfiguration.java
@@ -3,7 +3,9 @@ package com.tmobile.cdp.hello.configuration;
 import com.tmobile.cdp.hello.service.SecurityUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -23,6 +25,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService);
+	}
+
+	/**
+	 * Configures the endpoints which are non-secured (don't need any security).
+	 */
+	@Override
+	public void configure(WebSecurity webSecurity) {
+		webSecurity.ignoring()
+				.antMatchers(HttpMethod.GET, "/actuator/health/*");
 	}
 
 	@Bean
