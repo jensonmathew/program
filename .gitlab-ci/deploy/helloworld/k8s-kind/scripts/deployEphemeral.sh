104c89ba2aa7c4810dbccf7714b5e628244ad4fc
Thomas Joseph <thomas.joseph30@t-mobile.com>
2022-09-20 12:33:55 +0530

    ephemeral k8s deploy

---
+++ .gitlab-ci/deploy/helloworld/k8s-kind/scripts/deployEphemeral.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+#set -x
+
+# Exit if any issues
+set -e
+
+# shellcheck source=./tmo-common/shared.sh
+. "${TMO_SHARED_SH}"
+
+# Create Docker Pull Secret
+tmoLog "Setting secret for image pull"
+kubectl create secret docker-registry "tmobile-cdp-image-pull" --docker-server=registry.gitlab.com --docker-username="${REGISTRY_USERNAME}" --docker-password="${REGISTRY_PASSWORD}" --namespace=ingress-nginx
+
+tmoLog "Modifying host file for host resolution"
+tmoLogCollapsed "$(cat /etc/hosts)" "HOSTS FILE"
+dockerIp=`grep "docker " /etc/hosts | grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])'`
+tmoLog "dockerIp parsed from /etc/hosts:- $dockerIp"
+if [ -z "$dockerIp" ];then tmoLog "dockerIp parsing failed" "warn"; fi;
+echo "${dockerIp}       ${HELM_APP_NAME}.${K8S_DOMAIN}" >> /etc/hosts
+tmoLogCollapsed "$(cat /etc/hosts)" "MODIFIED HOSTS FILE"
+
+# Jinja parse values yaml
+if [ -f "${HELM_VALUES}".j2 ]; then
+   genConfigFileFromJ2 "${HELM_VALUES}".j2 "${HELM_VALUES}"
+   tmoLogCollapsed "$(cat "${HELM_VALUES}")" "GENERATED VALUES YAML FOR DEPLOYMENT"
+fi
+
+# Jinja parse chart yaml
+if [ -f "${HELM_CHART_DIR}"/Chart.yaml.j2 ]; then
+   genConfigFileFromJ2 "${HELM_CHART_DIR}"/Chart.yaml.j2 "${HELM_CHART_DIR}"/Chart.yaml
+fi
+
+# Helm chart installation
+tmoLog "Helloworld App Installation initiated" "tmo"
+helm upgrade --install $HELM_APP_NAME $HELM_CHART_DIR --namespace=ingress-nginx
+
+sleep 50
+echo "------------------------------------------------------------------------------------------"
+helm get manifest "$HELM_APP_NAME" --namespace=ingress-nginx 
+echo "------------------------------------------------------------------------------------------"
---
+++ .gitlab-ci/deploy/helloworld/k8s-kind/scripts/setupCluster.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+#set -x
+
+# Exit if any issues
+set -e
+
+# shellcheck source=./tmo-common/shared.sh
+. "${TMO_SHARED_SH}"
+
+if ! exists "curl" ; then installPackage "curl"; fi;
+
+# Install kubectl
+tmoLog "Installing kubectl" "debug"
+curl --progress-bar -LO https://storage.googleapis.com/kubernetes-release/release/$KUBECTL_VERSION/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mv ./kubectl /usr/local/bin/kubectl
+tmoLog "Installed kubectl             :   [ $(kubectl version --short) ]" "tmo"
+
+# Install KinD
+tmoLog "Installing kind" "debug"
+curl --progress-bar -Lo ./kind https://kind.sigs.k8s.io/dl/$KIND_VERSION/kind-linux-amd64
+chmod +x ./kind
+mv ./kind /usr/local/bin/kind
+tmoLog "Installed kind                :   [ $(kind version) ]" "tmo"
+
+# Install Helm
+tmoLog "Installing helm"
+curl --progress-bar -L https://get.helm.sh/helm-$HELM_VERSION-linux-amd64.tar.gz | tar -xz
+chmod +x ./linux-amd64/helm
+mv ./linux-amd64/helm /usr/local/bin/helm
+tmoLog "Installed helm                :   [ $(helm version --short) ]" "tmo"
+
+# Cluster creation
+tmoLog "Initiating KinD cluster"
+kind create cluster --name=cdp-hw --config=./config/kind-gitlab.yaml --wait 180s
+sed -i -E -e 's/localhost|0\.0\.0\.0/docker/g' "$HOME/.kube/config"
+tmoLog "KinD installation complete!!!"
+
+# Modify cluster to disable cert checking
+kubectl config set-cluster kind-cdp-hw --insecure-skip-tls-verify=true
+# kubectl config set-cluster kind-cdp-hw --certificate-authority=tmo-ca2-root.crt
+
+# Install IC
+tmoLog "Creating NameSpace and installing Nginx ingress controller"
+kubectl apply -f config/ingress-controller.yaml
+
+# Configure context to use newly created NS
+kubectl config set-context kind-cdp-hw --namespace=ingress-nginx 
+
+# Ensure all good before moving further
+kubectl wait --namespace ingress-nginx \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/component=controller \
+  --timeout=90s
+
