3e35a3e8dda557ce4e8358dc28bf24a4cf5065d6
Thomas Joseph <thomas.joseph30@t-mobile.com>
2022-09-22 03:31:46 +0530

    adding SA

--- .gitlab-ci/deploy/helloworld/k8s-kind/scripts/deployEphemeral.sh
+++ .gitlab-ci/deploy/helloworld/k8s-kind/scripts/deployEphemeral.sh
@@ -8,6 +8,31 @@ set -e
 # shellcheck source=./tmo-common/shared.sh
 . "${TMO_SHARED_SH}"
 
+# Fn to validate Canary Deployment
+validateAppDeployment(){
+    appRespFile="response.json"
+    helloworldBaseUrl="http://helloworld.ephemeral.ci"
+    deployApiUrl="${helloworldBaseUrl}/api/deploy/info"
+    status_code=$(curl --max-time 10 -s -H "Content-Type: application/json" -o "$appRespFile" -w "%{http_code}" "$deployApiUrl")
+    tmoLog "httpStatusCode: [ ${status_code} ] for [ ${deployApiUrl} ]" "debug"
+    if [ "$status_code" = 200 ]; then
+         tmoLogCollapsed "$(cat "${appRespFile}")" "Helloworld Api Response"
+         deployedVer=$(< "${appRespFile}" jq -r '.deployedVersion')
+         pipelineId=$(< "${appRespFile}" jq -r '.pipelineId')
+         commitSha=$(< "${appRespFile}" jq -r '.commitSha')
+         tmoLog "App RESP, Ver: [ ${deployedVer} ], Pipeline ID: [ ${pipelineId} ], SHA: [ ${commitSha} ]" "success"
+    else
+         tmoLog "Helloworld App Deploy API Status [ ${status_code} ]" "error"
+    fi
+}
+
+# Prepare necessary charts for ephemeral deployment 
+cp -r .gitlab-ci/deploy/helloworld/k8s-kind/templates/serviceaccount.yaml "${HELM_CHART_DIR}"/templates/serviceaccount.yaml
+
+echo " -- List of Files in [ ${HELM_CHART_DIR} ] --- "
+ls -R "${HELM_CHART_DIR}"
+echo " --------------------------------------------- "
+
 # Create Docker Pull Secret
 tmoLog "Setting secret for image pull"
 kubectl create secret docker-registry "tmobile-cdp-image-pull" --docker-server=registry.gitlab.com --docker-username="${REGISTRY_USERNAME}" --docker-password="${REGISTRY_PASSWORD}" --namespace=ingress-nginx
@@ -33,5 +58,13 @@ fi
 
 # Helm chart installation
 tmoLog "Helloworld App Installation initiated" "tmo"
-helm upgrade --install "$HELM_APP_NAME" "$HELM_CHART_DIR" --namespace=ingress-nginx
+helm upgrade --install "$HELM_APP_NAME" "$HELM_CHART_DIR" --namespace=ingress-nginx --wait
+
+sleep 5
+echo "------------------------------------------------------------------------------------------"
+helm get manifest "$HELM_APP_NAME" --namespace=ingress-nginx 
+echo "------------------------------------------------------------------------------------------"
+
+# Check if the app is available
+validateAppDeployment
 
