681772f8e0ca02cd63b1487efd46917592927249
jensonmathew <74042286+jensonmathew@users.noreply.github.com>
2025-08-02 16:20:40 +0530

    Update SecurityConfiguration.java

--- src/main/java/com/tmobile/hello/configuration/SecurityConfiguration.java
+++ src/main/java/com/tmobile/hello/configuration/SecurityConfiguration.java
@@ -1,5 +1,4 @@
 package com.tmobile.hello.configuration;
-
 import com.tmobile.hello.service.SecurityUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
